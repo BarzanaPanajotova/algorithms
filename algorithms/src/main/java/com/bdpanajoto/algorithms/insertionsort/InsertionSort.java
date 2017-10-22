7254b56e19b76fa6588d16056dc0714f8a488aa6
OracleInside <bdpanajotova@gmail.com>
2017-10-22 10:21:46 +0300

    Fix documentation

--- algorithms/src/main/java/com/bdpanajoto/algorithms/insertionsort/InsertionSort.java
+++ algorithms/src/main/java/com/bdpanajoto/algorithms/insertionsort/InsertionSort.java
@@ -3,10 +3,10 @@ package com.bdpanajoto.algorithms.insertionsort;
 import java.util.Objects;
 
 /**
- * This is implementation Insertion Sort in Java. The basic algorithm definition
- * is to sort the elements of the given array by going through every single
- * value of the array and inserting it at the right place in the preceding part
- * of the array.
+ * This is an implementation of Insertion Sort in Java. The basic algorithm
+ * definition is to sort the elements of the given array by going through every
+ * single value of the array and inserting it at the right place in the
+ * preceding part of the array.
  * 
  * The invariant of the algorithm is that the first part of the array that
  * consists of elements before the element we are currently looking at is always
@@ -21,10 +21,9 @@ public final class InsertionSort {
 	/**
 	 * First try in implementing Insertion Sort.
 	 * 
-	 * 
 	 * @param array
 	 *            sequence of unsorted integers.
-	 * @return new instance of a sorted representation of the input array.
+	 * @return the same array instance but sorted.
 	 */
 	public static final int[] sort(int[] array) {
 		Objects.requireNonNull(array);
